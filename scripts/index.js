84aafa1758af067fe89a402df6f269c4cc14e0ce
sagarsharma21 <007sharma.sagar@gmail.com>
2023-10-04 23:59:37 +0530

    Add JS functionality

---
+++ scripts/index.js
@@ -0,0 +1,33 @@
+const item = document.querySelector('#item');
+const todobox = document.querySelector('#to-do-box');
+const cross =document.getElementsByTagName('i');
+
+console.log(item +' Added to task list');
+
+item.addEventListener('keyup', function(event){
+     //console.log(event);
+     if(event.key == 'Enter'){
+        //console.log(this.value);
+        addToDo(this.value);
+        this.value = "";
+    }
+
+});
+
+const addToDo = (item) => {
+    const listItem =document.createElement("li");
+    listItem.innerHTML =  `${item}
+         <i class="fas fa-times"></i>`; 
+
+    listItem.addEventListener('click', function(){
+        this.classList.toggle("done");
+    }); 
+
+    listItem.querySelector('i').addEventListener(
+        'click', function(){
+            listItem.remove();
+        }
+    );
+    
+    todobox.appendChild(listItem);
+}
